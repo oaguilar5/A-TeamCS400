71862412ff8b07178f461e74a991aa92dd4ec544
oaguilar5 <36973812+oaguilar5@users.noreply.github.com>
2018-04-22 16:49:37 -0500

    Add files via upload
    updated main to properly reflect GUI size

--- GUI/src/application/Main.java
+++ GUI/src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,999,745);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
