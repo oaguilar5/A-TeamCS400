21e29f1b2b4705dc1652a016fae976d18f23185a
jahn36 <36972215+jahn36@users.noreply.github.com>
2018-04-23 21:14:11 -0500

    comments

--- Match.java
+++ Match.java
@@ -1,13 +1,27 @@
 
+/**
+ * The Match class has constructor for making a Match object and has a method
+ * for indicating who is the winner of the match. 
+ */
 public class Match {
-	Challenger c1;
-	Challenger c2;
+	private Challenger c1;
+	private Challenger c2;
 	
+	/**
+	 * The constructor for match object by receiving two challenger object
+	 * as a parameter
+	 * 
+	 * @param Challegner c1 -first challenger
+	 * @param Challenger c2 -second challenger
+	 */
 	public Match(Challenger c1, Challenger c2) {
 		this.c1 = c1;
 		this.c2 = c2;
 	}
 	
+	/**
+	 * This method shows the winner of the match object. 
+	 */
 	public Challenger getWinner() {
 		if(c1.getScore() > c2.getScore()) {
 			return c1;
